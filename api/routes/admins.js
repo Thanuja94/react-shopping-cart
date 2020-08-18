eaf7924c75b7752f57d896ed15ecd2a8eb635b37
Buddhimal Gunasekara <48013710+Buddhimal@users.noreply.github.com>
2020-08-18 23:49:00 +0530

    create get, get all, put for admin model

--- api/routes/admins.js
+++ api/routes/admins.js
@@ -3,25 +3,61 @@ const router = express.Router();
 const Admin = require('../models/admin');
 
 
-// router.post('/admin', async (req, res) => {
+router.post("/", async (req, res) => {
 
-    router.post("/", async (req, res) => {
+    try {
+        let admin = new Admin({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            isActive: req.body.isActive
+        });
 
-        try {
-            let admin = new Admin({
+        admin = await admin.save();
+        res.send(admin);
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
+
+});
+
+router.get('/', async(req, res) => {
+
+    try{
+        let admin = await Admin.find({});
+        res.send(admin);
+    }
+    catch(e){
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/:userId', async(req, res) => {
+    try {
+        let admin = await Admin.findOne({ _id: req.params.userId }
+        );
+        res.send(admin);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+
+router.put('/:userId', async(req, res) => {
+
+    //update first approach
+    let admin = await Admin.findOneAndUpdate({ _id: req.params.userId }, {
+            $set: {
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password,
                 isActive: req.body.isActive
-            });
+            }
+        }, { new: true, useFindAndModify: false }
 
-            admin = await admin.save();
-            res.send(admin);
-        } catch (e) {
-            return res.status(500).send(e.message);
-        }
+    );
+    res.send(await admin.save());
 
-    });
-// });
+});
 
 module.exports = router;
