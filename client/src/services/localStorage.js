677de84860e5b28fa0667a6005a04b8f255a554a
ChamodNimz <chamodnimsaradx@gmail.com>
2020-08-21 11:30:00 +0530

    frond-end services, config and session manager added

---
+++ client/src/services/localStorage.js
@@ -0,0 +1,32 @@
+const TOKEN_KEY = 'TOKEN'
+const REFRESH_TOKEN_KEY = ''
+
+const TokenService = {
+    getToken() {
+        return localStorage.getItem(TOKEN_KEY)
+    },
+
+    saveToken(accessToken) {
+        localStorage.setItem(TOKEN_KEY, accessToken)
+    },
+
+    removeToken() {
+        localStorage.removeItem(TOKEN_KEY)
+    },
+
+    getRefreshToken() {
+        return localStorage.getItem(REFRESH_TOKEN_KEY)
+    },
+
+    saveRefreshToken(refreshToken) {
+        localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken)
+    },
+
+    removeRefreshToken() {
+        localStorage.removeItem(REFRESH_TOKEN_KEY)
+    }
+
+}
+
+
+export { TokenService }
