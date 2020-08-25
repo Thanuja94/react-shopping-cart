adb40efa6533925573e14601a88e9b9e1bb1bdcb
Buddhimal Gunasekara <48013710+Buddhimal@users.noreply.github.com>
2020-08-25 06:41:34 +0530

    chanage admin UI

--- client/src/components/admin/AdminList.js
+++ client/src/components/admin/AdminList.js
@@ -22,56 +22,22 @@ class AdminList extends Component {
                                         <thead>
                                         <tr>
                                             <th scope="col">#</th>
-                                            <th scope="col">Projects</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Email</th>
                                             <th scope="col">Start Date</th>
                                             <th scope="col">Status</th>
-                                            <th scope="col">Team</th>
-                                            <th scope="col">Progress</th>
                                             <th scope="col">Action</th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr>
                                             <th scope="row">1</th>
-                                            <td>New admin Design</td>
+                                            <td>Buddhimal Gunasekara</td>
+                                            <td>buddhimal@gmail.com</td>
                                             <td>02/5/2019</td>
                                             <td>
                                                 <span className="text-success font-12"><i
-                                                    className="mdi mdi-checkbox-blank-circle mr-1"></i> Completed</span>
-                                            </td>
-                                            <td>
-                                                <div className="team">
-                                                    <a href="javascript: void(0);" className="team-member"
-                                                       data-toggle="tooltip" data-placement="top" title=""
-                                                       data-original-title="Roger Drake">
-                                                        <img src="https://bootdey.com/img/Content/avatar/avatar6.png"
-                                                             className="rounded-circle avatar-xs" alt=""/>
-                                                    </a>
-
-                                                    <a href="javascript: void(0);" className="team-member"
-                                                       data-toggle="tooltip" data-placement="top" title=""
-                                                       data-original-title="Reggie James">
-                                                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png"
-                                                             className="rounded-circle avatar-xs" alt=""/>
-                                                    </a>
-
-                                                    <a href="javascript: void(0);" className="team-member"
-                                                       data-toggle="tooltip" data-placement="top" title=""
-                                                       data-original-title="Gerald Mayberry">
-                                                        <img src="https://bootdey.com/img/Content/avatar/avatar8.png"
-                                                             className="rounded-circle avatar-xs" alt=""/>
-                                                    </a>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p className="mb-0">Progress<span className="float-right">100%</span>
-                                                </p>
-
-                                                <div className="progress mt-2" style={{"height": "5px"}}>
-                                                    <div className="progress-bar bg-success" role="progressbar"
-                                                         style={{"width": "100%"}} aria-valuenow="100" aria-valuemin="0"
-                                                         aria-valuemax="100"></div>
-                                                </div>
+                                                    className="mdi mdi-checkbox-blank-circle mr-1"></i> Active</span>
                                             </td>
 
                                             <td>
@@ -87,37 +53,12 @@ class AdminList extends Component {
                                         </tr>
                                         <tr>
                                             <th scope="row">2</th>
-                                            <td>Landing page Design</td>
-                                            <td>04/6/2019</td>
+                                            <td>Buddhimal Gunasekara</td>
+                                            <td>buddhimal@gmail.com</td>
+                                            <td>02/5/2019</td>
                                             <td>
-                                                <span className="text-primary font-12"><i
-                                                    className="mdi mdi-checkbox-blank-circle mr-1"></i> Pending</span>
-                                            </td>
-                                            <td>
-                                                <div className="team">
-                                                    <a href="javascript: void(0);" className="team-member"
-                                                       data-toggle="tooltip" data-placement="top" title=""
-                                                       data-original-title="Deborah Mixon">
-                                                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png"
-                                                             className="rounded-circle avatar-xs" alt=""/>
-                                                    </a>
-
-                                                    <a href="javascript: void(0);" className="team-member"
-                                                       data-toggle="tooltip" data-placement="top" title=""
-                                                       data-original-title="Scott Jessie">
-                                                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png"
-                                                             className="rounded-circle avatar-xs" alt=""/>
-                                                    </a>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p className="mb-0">Progress<span className="float-right">78%</span></p>
-
-                                                <div className="progress mt-2" style={{"height": "5px"}}>
-                                                    <div className="progress-bar bg-primary" role="progressbar"
-                                                         style={{"width": "78%"}} aria-valuenow="78" aria-valuemin="0"
-                                                         aria-valuemax="100"></div>
-                                                </div>
+                                                <span className="text-success font-12"><i
+                                                    className="mdi mdi-checkbox-blank-circle mr-1"></i> Active</span>
                                             </td>
 
                                             <td>
@@ -127,7 +68,7 @@ class AdminList extends Component {
                                                         className="fa fa-pencil h5 m-0"></i></a>
                                                     <a href="#" className="text-danger" data-toggle="tooltip"
                                                        data-placement="top" title="" data-original-title="Close"> <i
-                                                        className="fa fa fa-remove h5 m-0"></i></a>
+                                                        className="fa fa-remove h5 m-0"></i></a>
                                                 </div>
                                             </td>
                                         </tr>
