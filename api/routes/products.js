6c1666e07c4e62bc5b90c881873c40762793ff09
ChamodNimz <chamodnimsaradx@gmail.com>
2020-09-03 18:10:56 +0530

    minor changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,50 +14,6 @@ router.get('/products', async(req, res) => {
     } catch (e) {
         res.status(500).send(e.message);
     }
-});
-
-router.get('/products/:productId', async(req, res) => {
-    try {
-        let product = await Product.findOne({ _id: req.params.productId } //Products that are match with params id
-        );
-        if (!product) res.status(400).send('Product ID is not found!');
-        res.send(product);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
- 
-router.put('/products/:productId',productValidationRules(), validate , async(req, res) => {
-    
-    const file = req.files.file;
-try{
-    await file.mv(`../client/public/uploads/${file.name}`, function(err) {
-        if (err) {
-            console.log(err);
-            if (!file) res.status(400).send('Image should be uploaded ...');
-            res.status(500).send("Error uploading image...");  
-        }
-    });
-    
-    //update first approach   
-    let product = await Product.findOneAndUpdate({ _id: req.params.productId }, {
-            $set: {
-                name: req.body.productName,
-                price: req.body.price,
-                qty: req.body.qty,
-                category: req.body.category,
-                imagePath: file.imagePath
-            }
-        }, { new: true, useFindAndModify: false }
-
-    );
-    if (!product) res.status(400).send('Product ID is not found!');
-    res.send(await product.save());
-}
-   catch(e){
-    res.status(500).send(e.message);
-   }
-
 }); 
 
 router.post('/products', async(req, res) => {
@@ -102,23 +58,5 @@ router.post('/products', async(req, res) => {
     }
 
 });
-
-router.delete('/products/:productId',async(req,res)=>{
-    try{
-        let product = await Product.findByIdAndDelete(
-            {_id : req.params.productId}         
-            )
-            if (!product) res.status(400).send('Product ID is not found!');
-            res.send(product);
-        
-            }
-    
-    catch(e){
-        res.status(404).send(e);
-    }       
-
-});
-
-
 
 module.exports = router;
